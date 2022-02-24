4e211e0d961393f94462fb2a375e526c3abcc048
Jake Enger <99782876+jake-enger@users.noreply.github.com>
2022-02-24 16:19:56 -0700

    ?

--- lab.js
+++ lab.js
@@ -119,7 +119,15 @@ while (score < 7) {
 
 */
 
-var changeMyMind = true
+let changeMyMind = true
+
+if (changeMyMind = false) {
+  console.log("Im wrong.") }
+    else {
+    console.log("YOU'RE wrong")
+  }
+  
+
 
 ////////// PROBLEM 10 //////////
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
